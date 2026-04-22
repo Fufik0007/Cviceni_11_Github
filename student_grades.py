d3f3463a0870513f2e1a50ba3d86767ea100f149
Fufik0007 <jaroslav.urban@fyziomed.cz>
2026-04-22 10:53:46 +0200

    Grades_2

--- student_grades.py
+++ student_grades.py
@@ -1,3 +1,5 @@
+from main import random_numbers
+
 class StudentsGrades:
     def __init__(self, scores):
         self.scores = scores
@@ -49,17 +51,31 @@ class StudentsGrades:
             counter_while += 1
         return new_list
 
-if __name__ == "__main__":
+def main():
     results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    print(results.count)
+    for student_id in range(results.count()):
+        print(f"Student {student_id}: {results.get_by_index(student_id)} points – {results.get_grade(student_id)}")
+    print(f"Plny počet měli studenti: {results.find(100)}")
+    print(f"seznam vysledky: {results.get_sorted()}")
 
-    print(results.count())  # 9
-    print(results.get_by_index(2))  # 91
-    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
-    print(results.get_grade(2))  # A (91 bodů)
-    print(results.get_grade(6))  # A (100 bodů)
-    print(results.get_grade(7))  # F (38 bodů)
-    print(results.find(100))  # [6]
-    print(results.find(50))  # [4]
-    print(results.find(77))  # []
-    print(results.get_sorted())  # [38, 42, 50, 58, 67, 73, 85, 91, 100]
-    print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]  ← beze změny
+    random_results = StudentsGrades(random_numbers(30, 0, 100))
+    print(random_results.count())
+    print(random_results.get_sorted())
+
+
+if __name__ == "__main__":
+    main()
+    # results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    #
+    # print(results.count())  # 9
+    # print(results.get_by_index(2))  # 91
+    # print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+    # print(results.get_grade(2))  # A (91 bodů)
+    # print(results.get_grade(6))  # A (100 bodů)
+    # print(results.get_grade(7))  # F (38 bodů)
+    # print(results.find(100))  # [6]
+    # print(results.find(50))  # [4]
+    # print(results.find(77))  # []
+    # print(results.get_sorted())  # [38, 42, 50, 58, 67, 73, 85, 91, 100]
+    # print(results.scores)  # [85, 42, 91, 67, 50, 73, 100, 38, 58]  ← beze změny
